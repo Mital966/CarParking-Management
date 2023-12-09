a8416630e2f3a829ecc70d24c71a49b2d47ce6f5
GYANA PRASAD NAYAK <112857512+Mital966@users.noreply.github.com>
2023-12-10 00:06:51 +0530

    hyscaler examination

---
+++ HyScalar/src/main/java/tech/csm/service/PlaceService.java
@@ -0,0 +1,18 @@
+package tech.csm.service;
+
+import java.util.List;
+
+import tech.csm.model.Place;
+
+public interface PlaceService {
+
+	List<Place> findAllPlaces();
+
+	Place findById(Integer pId);
+
+	Place addSeatsToPlace(Place place);
+
+	Place saveBookedPlace(Place findById);
+
+
+}
---
+++ HyScalar/src/main/java/tech/csm/service/PlaceServiceImpl.java
@@ -0,0 +1,41 @@
+package tech.csm.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tech.csm.model.Place;
+import tech.csm.repository.PlaceServiceRepository;
+
+@Service
+public class PlaceServiceImpl implements PlaceService{
+	
+	@Autowired
+	private PlaceServiceRepository PlaceServiceRepository;
+
+	@Override
+	public List<Place> findAllPlaces() {
+		return PlaceServiceRepository.findAll();
+	}
+
+	@Override
+	public Place findById(Integer pId) {
+		return PlaceServiceRepository.findById(pId).get();
+	}
+
+
+	@Override
+	public Place addSeatsToPlace(Place place) {
+		return PlaceServiceRepository.save(place);
+	}
+
+	@Override
+	public Place saveBookedPlace(Place place) {
+		place.setSeats(place.getSeats()-1);
+		return PlaceServiceRepository.save(place);
+	}
+
+	
+
+}
